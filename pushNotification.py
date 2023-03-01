2490a940b9bc2c0a7c54aafa3096cefdeb149ac3
Matthew Madairy <mattmadairy@gmail.com>
2023-03-01 18:09:15 -0500

    pushNotification.py

---
+++ pushNotification.py
@@ -0,0 +1,16 @@
+import http.client, urllib
+
+def pushOver(message):
+  conn = http.client.HTTPSConnection("api.pushover.net:443")
+
+  conn.request("POST", "/1/messages.json",
+
+    urllib.parse.urlencode({
+    "token": "aobd9y8u2nyerhprujnkwesdnf6sos",
+    "user": "ugn1u7k4hgcv6841jdff2wfux3nc8r",
+    "message": message,
+    }), { "Content-type": "application/x-www-form-urlencoded" })
+
+  conn.getresponse()
+
+pushOver('message')
